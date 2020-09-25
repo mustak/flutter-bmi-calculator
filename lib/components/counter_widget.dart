0f6ee35807a71f5413fa6c9e9fcc26e7437d264b
mustak <mhyaqub@yahoo.com>
2020-09-25 22:45:05 +0100

    Remove unused import constants

--- lib/components/counter_widget.dart
+++ lib/components/counter_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bmi_calculator/constants.dart';
 import 'package:flutter_bmi_calculator/base/button_icon.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter_bmi_calculator/base/value_component.dart';
